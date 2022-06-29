8a40e2b035d448c5615e0879d36e4511f1b96f1e
Liz alves <elisabete_carente@hotmail.com>
2022-06-29 10:28:09 -0300

    adicionando o arquivo do postman

--- pokedex/src/routes/Routes.js
+++ pokedex/src/routes/Routes.js
@@ -2,16 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ErroPage from "../pages/Error/ErrorPage";
 import HomePage from "../pages/Home/HomePage";
 import PoquedexPage from "../pages/Pokedex/PokedexPage"
-import PokemonDetailsPage from"../pages/PokemonDetails/PokemonDetailsPage"
+import PokemonDetailsPage from "../pages/PokemonDetails/PokemonDetailsPage"
 
 export function Router() {
    return (
       <BrowserRouter>
          <Routes>
-            <Route path='*' element={<ErroPage />} />
             <Route index element={<HomePage />} />
             <Route path='pokedexpage' element={<PoquedexPage />} />
             <Route path='pokemondetails' element={<PokemonDetailsPage />} />
+            <Route path='*' element={<ErroPage />} />
          </Routes>
       </BrowserRouter>
    )
